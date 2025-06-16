f1df25b825055ce5c0ae0d10c0579a2ac2bf76eb
Maike <maikonalexandre574@gmail.com>
2025-06-15 22:30:54 -0300

    feat: atualizar documentação da API com novos endpoints de autenticação e ajustes na configuração do Swagger

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -3,11 +3,23 @@ const YAML = require("yamljs");
 const path = require("path");
 
 const base = YAML.load(path.join(__dirname, "/base.yml"));
+const components = YAML.load(path.join(__dirname, "/components.yaml"));
 const auth = YAML.load(path.join(__dirname, "/paths/auth.yaml"));
+const usuarios = YAML.load(path.join(__dirname, "/paths/usuarios.yaml"));
+
+const controleAlteracao = YAML.load(
+  path.join(__dirname, "/paths/controleAlteracao.yaml")
+);
+
+base.components = {
+  ...components,
+};
 
 // Unir paths
 base.paths = {
   ...auth,
+  ...controleAlteracao,
+  ...usuarios
 };
 
 module.exports = base;
